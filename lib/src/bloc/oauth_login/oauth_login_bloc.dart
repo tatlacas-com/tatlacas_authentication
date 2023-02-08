3999b3bda175be2ba9a35fd414d36f7b5ae51120
Tatenda Caston Hove <tathove@tatlacas.com>
2023-02-08 09:24:14 +0200

    remove unused repo

--- lib/src/bloc/oauth_login/oauth_login_bloc.dart
+++ lib/src/bloc/oauth_login/oauth_login_bloc.dart
@@ -6,30 +6,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:tatlacas_authentication/src/bloc/authentication/auth_bloc.dart';
 import 'package:tatlacas_authentication/src/model/user_entity.dart';
-import 'package:tatlacas_authentication/src/repo/oauth_repo.dart';
 import 'package:tatlacas_authentication/src/repo/user_repo.dart';
-import 'package:tatlacas_flutter_oauth/app_auth_export.dart';
-import 'package:universal_platform/universal_platform.dart';
 
 part 'oauth_login_event.dart';
 
 part 'oauth_login_state.dart';
 
-abstract class OauthLoginBloc<TRepo extends OauthRepo,
-    TAuthBloc extends AuthBloc> extends Bloc<OauthLoginEvent, OauthLoginState> {
+abstract class OauthLoginBloc<TAuthBloc extends AuthBloc>
+    extends Bloc<OauthLoginEvent, OauthLoginState> {
   @protected
   final TAuthBloc authBloc;
   @protected
   final UserRepo userRepo;
-  @protected
-  final TRepo oauthRepo;
 
   bool get initialAuth => authBloc.state.initialAuth;
 
   OauthLoginBloc({
     required this.authBloc,
     required this.userRepo,
-    required this.oauthRepo,
   }) : super(OauthLoginInitial()) {
     on<OauthLoginRequestedEvent>(onOauthLoginRequested);
     on<RetryRequestedEvent>(onRetryRequested);
@@ -48,52 +42,7 @@ abstract class OauthLoginBloc<TRepo extends OauthRepo,
 
   @protected
   FutureOr<void> onOauthLoginRequested(
-      OauthLoginRequestedEvent event, Emitter<OauthLoginState> emit) async {
-    try {
-      authBloc.add(ChangeAuthStatusEvent(
-        status: AuthenticationStatus.authenticating,
-        authType: event.authType,
-        initialAuth: event.initialAuth,
-      ));
-      emit(const OauthLoginInProgress());
-      if (UniversalPlatform.isWeb) {
-      } else {
-        var authResponse = await oauthRepo.authenticate(
-          event.authType,
-          params: event.params,
-        );
-        await createUserFromResponse(event, authResponse, emit);
-      }
-    } catch (e) {
-      debugPrint(e.toString());
-      await onFailed(emit, event, e);
-    }
-  }
-
-  Future createUserFromResponse(
-      OauthLoginRequestedEvent event,
-      AuthorizationTokenResponse? authResponse,
-      Emitter<OauthLoginState> emit) async {
-    UserEntity? user;
-    if (authResponse != null) user = await createUser(event, authResponse);
-    if (user != null) {
-      var proceed = await onAuthSuccess(emit, event, user);
-      if (!proceed) {
-        await onFailed(emit, event, null);
-        return;
-      }
-      user = await userRepo.saveUser(user);
-      await Future.delayed(Duration(milliseconds: 500));
-      authBloc.add(ChangeAuthStatusEvent(
-        initialAuth: event.initialAuth,
-        status: AuthenticationStatus.authenticated,
-        user: user,
-        authType: event.authType,
-      ));
-    } else {
-      await onFailed(emit, event, null);
-    }
-  }
+      OauthLoginRequestedEvent event, Emitter<OauthLoginState> emit);
 
   @protected
   FutureOr<void> onFailed(Emitter<OauthLoginState> emit,
@@ -112,8 +61,4 @@ abstract class OauthLoginBloc<TRepo extends OauthRepo,
     emit(const OauthLoginSucceeded());
     return true;
   }
-
-  @protected
-  Future<UserEntity?> createUser(
-      OauthLoginRequestedEvent event, AuthorizationTokenResponse response);
 }
--- lib/tatlacas_authentication.dart
+++ lib/tatlacas_authentication.dart
@@ -6,5 +6,4 @@ export 'src/bloc/oauth_login/oauth_login_bloc.dart';
 export 'src/model/user_entity.dart';
 export 'src/repo/user_repo.dart';
 
-export 'src/repo/oauth_repo.dart';
 export 'src/model/oauth_config.dart';
